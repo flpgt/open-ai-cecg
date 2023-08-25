82b8a54a9fd7ac0f389d5c9e25ae96afaba2beb8
cecg06 <felipe.guette@escola.pr.gov.br>
2023-08-25 08:25:39 -0300

    Init

---
+++ src/main/java/br/com/open/ai/cecg/OpeinAi/OpeinAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.open.ai.cecg.OpeinAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpeinAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpeinAiApplication.class, args);
+	}
+
+}
